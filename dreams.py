d94af51cddceff1bde75e520a16cd88b11db8173
Raveline <eraveline@gmail.com>
2013-08-21 11:25:55 +0200

    Added every admin functions. Need to add pagination.

--- dreams.py
+++ dreams.py
@@ -14,25 +14,33 @@ app.config.from_object(config)
 
 _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
 
+### PAGES ###
+
 # Basic
 @app.route('/')
 def main_page():
-    return show_dreams(0)
+    return show_dreams()
 
-@app.route('/dreams/<int:from_nb>')
-def show_dreams(from_nb):
-    cur = g.db.execute('select dr_title, dr_text, dr_date from dreams order by dr_date desc LIMIT ?, ?', [from_nb, app.config['USER_PAGE_SIZE']])
-    dreams = [dict(title=row[0], text=row[1], date=row[2]) for row in cur.fetchall()]
-
+# Main page with pagination
+@app.route('/<int:from_nb>')
+def show_dreams(from_nb=0):
+    dreams = [dict(iddream = row[0], date=row[1], title=row[2], text=row[3]) for row in get_dream_page(from_nb)]
     pagination = paginate(from_nb, app.config['USER_PAGE_SIZE'], get_max_dreams())
     return render_template('dreams.html', 
             dreams=dreams, 
             previous_page = pagination[0],
             next_page = pagination[1])
 
-###############
-# Admin stuff #
-###############
+# Single dream page
+@app.route('/dream/<int:dream_id>')
+def show_dream(dream_id):
+    dream = get_dream_dict(dream_id)
+    if dream == None:
+        abort(404)
+    else:
+        return render_template('single_dream.html', dream=dream)
+
+### ADMIN PAGES ###
 
 # Login
 @app.route('/login', methods=['GET'])
@@ -41,7 +49,7 @@ def login_page():
 
 @app.route('/login', methods=['POST'])
 def login():
-    if session['logged_in']:
+    if session.get('logged_in'):
         return redirect(url_for('admin'))
     # Check if user is connected
     login = request.form['login']
@@ -62,24 +70,50 @@ def logout():
 @app.route('/admin')
 def admin():
     if not session.get('logged_in'):
-        abort(401)
+        return redirect(url_for('login'))
     else:
         return render_template('admin.html', dreams=get_admin_dreams())
 
 # Add dream
-@app.route('/new', methods=['POST', 'GET'])
+@app.route('/new', methods=['GET'])
+def new_dream():
+    if not session.get('logged_in'):
+        abort(401)
+    return render_template('new.html', dream = get_empty_dream_dict)
+
+@app.route('/new', methods=['POST'])
 def add_dream():
     if not session.get('logged_in'):
         abort(401)
-    elif request.method == 'GET':
-        today = time.strftime( "%d/%m/%Y" )
-        return render_template('new.html', today=today, title="", content="")
-    else:
-        g.db.execute('insert into dreams (dr_title, dr_text, dr_date) values (?,?,?)', [request.form['title'], request.form['content'], request.form['date']])
-        g.db.commit()
-        return redirect(url_for('admin'))
+    g.db.execute('insert into dreams (dr_title, dr_text, dr_date) values (?,?,?)', [request.form['title'], request.form['content'], request.form['date']])
+    g.db.commit()
+    return redirect(url_for('admin'))
 
-# Interceptors
+@app.route('/remove/<int:dream_id>')
+def remove_dream(dream_id):
+    if not session.get('logged_in'):
+        abort(401)
+    delete_dream(dream_id)
+    return redirect(url_for('admin'))
+
+@app.route('/modifiy/<int:dream_id>', methods=['GET'])
+def modify_dream(dream_id):
+    if not session.get('logged_in'):
+        abort(401)
+    dream = get_dream_dict(dream_id)
+    if dream == None:
+        abort(404)
+    return render_template('new.html', dream = dream)
+
+@app.route('/modify/<int:dream_id>', methods=['POST'])
+def act_modify_dream(dream_id):
+    if not session.get('logged_in'):
+        abort(401)
+    update_dream(dream_id, request.form['title'],
+            request.form['date'], request.form['content'])
+    return redirect(url_for('admin'))
+
+### INTERCEPTORS ###
 
 @app.context_processor
 def inject_globals():
@@ -109,21 +143,36 @@ def nl2br(eval_ctx, value):
         result = Markup(result)
     return result
 
+def get_empty_dream_dict():
+    return dict(dreamid = None, title="", 
+            date=time.strftime("%d/%m/%Y"), content="")
+
+def get_dream_dict(dream_id):
+    dream = get_single_dream(dream_id)
+    if dream is None:
+        return None
+    row = dream.fetchall()[0]
+    return dict(dreamid = row[0], title=row[1], date=row[2], content=row[3])
 
 # DB shortcuts
 
 def get_admin_dreams():
-    cur = g.db.execute('select dr_title, dr_id from dreams order by dr_date desc');
-    return [dict(title=row[0], date=row[1]) for row in cur.fetchall()]
+    cur = g.db.execute('select dr_date, dr_title, dr_id from dreams order by dr_date desc');
+    return [dict(date=row[0], title=row[1], iddream=row[2]) for row in cur.fetchall()]
 
 def get_single_dream(dream_id):
-    return g.db.execute('select dr_title, dr_date, dr_text from dreams where dr_id = ?', dream_id)
+    return g.db.execute('select dr_id, dr_title, dr_date, dr_text from dreams where dr_id = ?', [dream_id])
+
+def get_dream_page(from_nb):
+    return g.db.execute('select dr_id, dr_date, dr_title, dr_text from dreams order by dr_date desc LIMIT ?, ?', [from_nb, app.config['USER_PAGE_SIZE']])
 
 def update_dream(dream_id, title, date, text):
-    g.db.execute('update dreams set dr_title = ?, dr_date = ?, dr_text = ? where dr_id=?', title, date, text, dream_id)
+    g.db.execute('update dreams set dr_title = ?, dr_date = ?, dr_text = ? where dr_id=?', [title, date, text, dream_id])
+    g.db.commit()
 
 def delete_dream(dream_id):
-    g.db.execute('delete from dreams where dr_id = ?', dream_id)
+    g.db.execute('delete from dreams where dr_id = ?', [dream_id])
+    g.db.commit()
 
 def get_max_dreams():
     return g.db.execute('select count(*) from dreams').fetchall()[0][0]
